95471d848848531606b12390c03345fc69620812
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2023-10-10 00:07:46 +0200

    Development

--- mods/math/CornerRectangle.ts
+++ mods/math/CornerRectangle.ts
@@ -17,3 +17,11 @@ export function intersectsNonStrict(r1: CornerRectangle, r2: CornerRectangle): b
     r1.y1 < r2.y2
   );
 }
+
+export function getIntersection(r1: CornerRectangle, r2: CornerRectangle): CornerRectangle{
+  const x1 = Math.max(r1.x1, r2.x1);
+  const y1 = Math.max(r1.y1, r2.y1);
+  const x2 = Math.min(r1.x2, r2.x2);
+  const y2 = Math.min(r1.y2, r2.y2);
+  return { x1, y1, x2, y2 };
+}
