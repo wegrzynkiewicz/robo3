e221dc28d8f1e76ea6ece43e29283a6d219cba25
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2023-10-14 14:08:25 +0200

    Development

--- mods/client/src/graphic/tiles/TilesSceneBuilder.ts
+++ mods/client/src/graphic/tiles/TilesSceneBuilder.ts
@@ -4,7 +4,8 @@ import { registerService, ServiceResolver } from "../../../../core/dependency/se
 import { index2coords } from "../../../../core/numbers.ts";
 import { SCREEN_MAX_VISIBLE_TILE_Y, SCREEN_MAX_VISIBLE_TILE_X } from "../../../../core/vars.ts";
 import { Chunk, ChunkManager, chunkManagerService } from "../../../../domain-client/chunk/chunkManager.ts";
-import { cornerRect, intersectsNonStrict } from "../../../../math/CornerRectangle.ts";
+import { intersectsNonStrict } from "../../../../math/CornerRectangle.ts";
+import { Point } from "../../../../math/Point.ts";
 import { DynamicDrawBuffer } from "../DynamicDrawBuffer.ts";
 import { Viewport, viewportService } from "../Viewport.ts";
 import { tilesBufferService } from "./tilesBuffer.ts";
@@ -15,19 +16,23 @@ export class TilesSceneBuilder {
   public readonly depthMap: Uint8Array;
   public readonly performance = new PerformanceCounter('scene-builder', 60);
   public readonly layers: Uint8Array[];
+  protected paintedLayerCount = 0;
+  protected cellCountPerLayer = 0;
+  protected paintedDepthCellCount = 0;
+  protected layerSize: Point;
   public constructor(
     public readonly chunkManager: ChunkManager,
     public readonly viewport: Viewport,
     public readonly tilesBuffer: DynamicDrawBuffer,
   ) {
-    this.depthMap = new Uint8Array(
-      (SCREEN_MAX_VISIBLE_TILE_Y) * (SCREEN_MAX_VISIBLE_TILE_X)
-    );
+    this.layerSize = { x: SCREEN_MAX_VISIBLE_TILE_X, y: SCREEN_MAX_VISIBLE_TILE_Y };
+    this.cellCountPerLayer = (SCREEN_MAX_VISIBLE_TILE_Y) * (SCREEN_MAX_VISIBLE_TILE_X);
+    this.depthMap = new Uint8Array(this.cellCountPerLayer);
 
     this.layers = [
-      new Uint8Array((SCREEN_MAX_VISIBLE_TILE_Y) * (SCREEN_MAX_VISIBLE_TILE_X)),
-      new Uint8Array((SCREEN_MAX_VISIBLE_TILE_Y) * (SCREEN_MAX_VISIBLE_TILE_X)),
-      new Uint8Array((SCREEN_MAX_VISIBLE_TILE_Y) * (SCREEN_MAX_VISIBLE_TILE_X)),
+      new Uint8Array(this.cellCountPerLayer),
+      new Uint8Array(this.cellCountPerLayer),
+      new Uint8Array(this.cellCountPerLayer),
     ];
   }
 
@@ -89,23 +94,43 @@ export class TilesSceneBuilder {
     for (let y = tilesRect.y1; y < tilesRect.y2; y++) {
       let dstX = 0;
       for (let x = tilesRect.x1; x < tilesRect.x2; x++) {
-        const chunkX = Math.floor(x / 32);
-        const chunkY = Math.floor(y / 32);
-        const chunk = chunks?.[chunkY]?.[chunkX];
-        if (chunk) {
-          const tileX = x % 32;
-          const tileY = y % 32;
-          const srcIndex = tileY * 32 + tileX;
-          const dstIndex = dstY * SCREEN_MAX_VISIBLE_TILE_X + dstX;
-          const value = chunk.segment!.grid.view[srcIndex];
-          if (this.depthMap[dstIndex] === 0 && value < 10) {
-            this.depthMap[dstIndex] = z + 1;
+        const dstIndex = dstY * SCREEN_MAX_VISIBLE_TILE_X + dstX;
+        if (this.depthMap[dstIndex] === 0) {
+          const chunkX = Math.floor(x / 32);
+          const chunkY = Math.floor(y / 32);
+          const chunk = chunks?.[chunkY]?.[chunkX];
+          if (chunk) {
+            const tileX = x % 32;
+            const tileY = y % 32;
+            const srcIndex = tileY * 32 + tileX;
+            const value = chunk.segment!.grid.view[srcIndex];
+            if (value < 10) {
+              this.depthMap[dstIndex] = z + 1;
+              this.paintedDepthCellCount++;
+            }
+            buffer[dstIndex] = value;
+          } else {
+            this.depthMap[dstIndex] = 1;
+            this.paintedDepthCellCount++;
           }
-          buffer[dstIndex] = value;
         }
         dstX++;
       }
       dstY++;
+    }
+  }
+
+  public processLayer(z: number) {
+    const { tilesRect } = this.viewport;
+    let startX = tilesRect.x1;
+    let startY = tilesRect.y1;
+    const layer = this.layers[z];
+
+    for (let y = 0; y < this.layerSize.y; y++) {
+      for (let x = 0; x < this.layerSize.x; x++) {
+        const index = y * this.layerSize.x + ;
+        const value = layer;
+      }
     }
   }
 
@@ -118,9 +143,19 @@ export class TilesSceneBuilder {
     let index = 0;
     this.depthMap.fill(0);
 
+    this.paintedLayerCount = 0;
+    this.paintedDepthCellCount = 0;
     const end = Math.min(2, layer);
     for (let z = 0; z <= end; z++) {
       this.buildLayer(z);
+      this.paintedLayerCount++;
+      if (this.paintedDepthCellCount === this.cellCountPerLayer) {
+        break;
+      }
+    }
+
+    for (let z = this.paintedLayerCount; z > 0; z--) {
+      this.processLayer(z);
     }
 
     this.performance.end();
