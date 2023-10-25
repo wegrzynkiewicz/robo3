56764c9da307d96cae631d6e731b60f006e7eb87
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2023-10-25 21:33:26 +0200

    Development

--- mods/bench/deconstructive.ts
+++ mods/bench/deconstructive.ts
@@ -1,10 +1,10 @@
 const cells = new Uint16Array(9);
 
-Deno.bench("deconstructive", () => {
+Deno.bench("typed array deconstructive", () => {
   const [vq, vw, ve, va, _vs, vd, vz, vx, vc] = cells;
 });
 
-Deno.bench("line-by-line", () => {
+Deno.bench("typed array line-by-line", () => {
   const vq = cells[0];
   const vw = cells[1];
   const ve = cells[2];
@@ -14,4 +14,17 @@ Deno.bench("line-by-line", () => {
   const vz = cells[6];
   const vx = cells[7];
   const vc = cells[8];
+});
+
+const obj = { x: 1, y: 2, z: 3, w: 4 };
+
+Deno.bench("object deconstructive", () => {
+  const { x, y, z, w } = obj;
+});
+
+Deno.bench("object line-by-line", () => {
+  const x = obj.x;
+  const y = obj.y;
+  const z = obj.z;
+  const w = obj.w;
 });
