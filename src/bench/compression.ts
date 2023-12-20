d1a92c23fd0561e38f2ef219d16d3a6dbefe6572
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2023-12-20 14:18:53 +0100

    Development

--- src/bench/compression.ts
+++ src/bench/compression.ts
@@ -1,20 +1,17 @@
 import { deflate } from "../common/storage/deps.ts";
 
-const u16 = new Uint16Array(32 * 32 * 64);
-const u16v = new Uint8Array(u16.buffer);
-for (const i of u16.keys()) {
-  u16[i] = Math.random() * 64;
+const u16a = new Uint16Array(32 * 32 * 1);
+const u16v = new Uint8Array(u16a.buffer)
+for (let i = 0; i < u16a.length / 2; i++) {
+  u16a[i] = Math.random() * 32;
 }
-
-const u8 = new Uint8Array(32 * 32 * 64);
-const u8v = new Uint8Array(u8.buffer);
-for (const i of u8.keys()) {
-  u8[i] = Math.random() * 64;
-}
-
 const o16 = deflate(u16v);
-const o8 = deflate(u8v);
+console.log('u16: ', u16v.length, 'o16: ', o16.length);
 
-console.log(u16.length * 2, '-', u8.length, '=', u16.length * 2 - u8.length);
-console.log(o16.length, '-', o8.length, '=', o16.length - o8.length);
-console.log(u16.length * 2 - o16.length, '-', u8.length - o8.length, '=');
+const u8a = new Uint8Array(32 * 32 * 1);
+const u8v = new Uint8Array(u8a.buffer);
+for (let i = 0; i < u8a.length / 2; i++) {
+  u8a[i] = Math.random() * 32;
+}
+const o8 = deflate(u8v);
+console.log('u8: ', u8v.length, 'o8: ', o8.length);
